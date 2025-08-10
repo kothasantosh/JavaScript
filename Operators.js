f4ab535f992761747b52f67abab1bd0ae19eadff
kotha santosh <kothasantosh62@gmail.com>
2025-08-11 01:43:13 +0530

    feat: pusing javascript files

--- Operators.js
+++ Operators.js
@@ -6,3 +6,15 @@ var result = x - y
 console.log("Subtraction = " + result);
 var result = x * y
 console.log("Multiplication = " + result);
+
+let exponent = 5 ** 3; // 2 raised to the power of 3
+console.log("Exponentiation = " + exponent);
+
+
+
+
+/****************** logical operators ******************/
+let a = true, b = false; 
+
+console.log("Logical AND = " + (a && b)); // false
+console.log("Logical OR = " + (a || b)); // true    
