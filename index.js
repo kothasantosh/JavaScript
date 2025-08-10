77e0f044ece8fb1b7d31c62a3bad95d397528370
kotha santosh <kothasantosh62@gmail.com>
2025-08-10 21:16:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const a =5, b=4
+console.log("sum = " +a+b);
+
+console.log(3 + 3); 
+console.log("3" + "3"); 
+console.log("3" + + 3); 
+console.log(3 + + "3"); 
+console.log(3 + 3 - 3); 
+console.log("3" + "3" - "3"); 
