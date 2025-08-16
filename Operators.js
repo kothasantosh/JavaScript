98ef2bcdd5afc28a1e54d6ad8ffca4785b4a1233
kotha santosh <kothasantosh62@gmail.com>
2025-08-16 23:55:09 +0530

    operators

---
+++ Operators.js
@@ -0,0 +1,35 @@
+let x =5, y =20
+
+var result = x + y
+console.log("Addition = " + result);
+var result = x - y
+console.log("Subtraction = " + result);
+var result = x * y
+console.log("Multiplication = " + result);
+
+let exponent = 5 ** 3; // 2 raised to the power of 3
+console.log("Exponentiation = " + exponent);
+
+
+
+
+/****************** logical operators ******************/
+let a = true, b = false; 
+
+console.log("Logical AND = " + (a && b)); // false
+console.log("Logical OR = " + (a || b)); // true    
+
+console.log( (10 < 5) && (5 < 10) ); 
+console.log( (10 > 5) || (5 > 10) ); 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let myList = fruits.toString();
+console.log("Fruits List = " + myList); // Banana,Orange,Apple,Mango
+
+let m =1, n=2
+
+console.log( m & n ); // Bitwise AND
+console.log( m ^ n ); // Bitwise XOR
+console.log( m | n ); // Bitwise OR
+
+
